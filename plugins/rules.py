c397a4066cd7478c0c7b0d04785e91468df91d8a
Ansh_Suryavanshi <suryavanshiansh2007@gmail.com>
2025-07-01 02:26:20 +0530

    Update rules.py

--- plugins/rules.py
+++ plugins/rules.py
@@ -1,1 +1,14 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+RULES = """
+📜 Group Rules:
+1. Be respectful
+2. No links/spam
+3. Follow admin instructions
+"""
+
+@Client.on_message(filters.command("rules"))
+async def show_rules(client: Client, message: Message):
+    await message.reply(RULES.strip())
 
