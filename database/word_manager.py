96bece23c6ca27f7b11bcf9b282d8eda1734a453
Ansh_Suryavanshi <suryavanshiansh2007@gmail.com>
2025-07-01 02:04:57 +0530

    Update word_manager.py

--- database/word_manager.py
+++ database/word_manager.py
@@ -1,1 +1,19 @@
+# database/word_manager.py
 
+from .mongo import db
+
+badwords_col = db["badwords"]
+flood_col = db["floodwords"]
+
+def add_word(chat_id: int, word: str, word_type: str):
+    col = badwords_col if word_type == "bad" else flood_col
+    if not col.find_one({"chat_id": chat_id, "word": word}):
+        col.insert_one({"chat_id": chat_id, "word": word})
+
+def remove_word(chat_id: int, word: str, word_type: str):
+    col = badwords_col if word_type == "bad" else flood_col
+    col.delete_one({"chat_id": chat_id, "word": word})
+
+def get_words(chat_id: int, word_type: str):
+    col = badwords_col if word_type == "bad" else flood_col
+    return [x["word"] for x in col.find({"chat_id": chat_id})]
