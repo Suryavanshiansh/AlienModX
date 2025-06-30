08d2681781765062cf4018d8da02cb6001f64729
Ansh_Suryavanshi <suryavanshiansh2007@gmail.com>
2025-07-01 02:26:37 +0530

    Update schedule.py

--- plugins/schedule.py
+++ plugins/schedule.py
@@ -1,1 +1,7 @@
+from pyrogram import Client, filters
+from datetime import datetime
+
+@Client.on_message(filters.command("schedule"))
+async def schedule_msg(client: Client, message: Message):
+    await message.reply(f"📅 Server time is {datetime.now():%Y-%m-%d %H:%M:%S}")
 
