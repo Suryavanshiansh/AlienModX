f98925c94f361a427b6ee83e80a1c0dd9dd72104
Ansh_Suryavanshi <suryavanshiansh2007@gmail.com>
2025-07-01 02:12:10 +0530

    Update admin_check.py

--- filters/utils/admin_check.py
+++ filters/utils/admin_check.py
@@ -1,1 +1,9 @@
 
+from pyrogram.types import Message
+from config import OWNER_ID
+
+# 🔐 Admin Check Filter
+async def is_admin(client, message: Message) -> bool:
+    member = await client.get_chat_member(message.chat.id, message.from_user.id)
+    return member.status in ("administrator", "creator") or message.from_user.id == OWNER_ID
+
