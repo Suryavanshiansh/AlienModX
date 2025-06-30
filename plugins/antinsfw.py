80afe4af771000c73542475714a5792b835127ce
Ansh_Suryavanshi <suryavanshiansh2007@gmail.com>
2025-07-01 02:25:25 +0530

    Update antinsfw.py

--- plugins/antinsfw.py
+++ plugins/antinsfw.py
@@ -1,1 +1,12 @@
+from pyrogram import Client, filters
+from filters.utils.admin_check import is_admin
+
+NSFW_WORDS = ["nsfwword1", "nsfwword2"]
+
+@Client.on_message(filters.group & ~filters.create(is_admin))
+async def anti_nsfw(client, message):
+    text = message.text or ""
+    if any(w in text.lower() for w in NSFW_WORDS):
+        await message.delete()
+        await message.reply_text("🛑 NSFW content is not allowed here.")
 
