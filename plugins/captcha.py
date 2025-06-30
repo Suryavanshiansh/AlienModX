3a6fc6fd9e6efa589c6b1e08dbb13f76cd7bc93e
Ansh_Suryavanshi <suryavanshiansh2007@gmail.com>
2025-07-01 02:26:01 +0530

    Update captcha.py

--- plugins/captcha.py
+++ plugins/captcha.py
@@ -1,1 +1,8 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+@Client.on_message(filters.new_chat_members)
+async def welcome_verify(client: Client, message: Message):
+    new = message.new_chat_members[0]
+    await message.reply(f"👋 Welcome, {new.first_name}! Please type `/verify` to verify yourself.")
 
