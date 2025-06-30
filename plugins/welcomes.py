32fd9f021cbb0e15b065d16c66128eab32abf397
Ansh_Suryavanshi <suryavanshiansh2007@gmail.com>
2025-07-01 02:27:50 +0530

    Update welcomes.py

--- plugins/welcomes.py
+++ plugins/welcomes.py
@@ -1,1 +1,8 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+@Client.on_message(filters.new_chat_members)
+async def welcomes(client: Client, message: Message):
+    for user in message.new_chat_members:
+        await message.reply(f"👋 Welcome to the group, {user.first_name}!")
 
